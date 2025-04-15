b94906f6aa9c7ea4ecec66a791b66080f7866394
Ersin Güvenç <eguvenc@gmail.com>
2025-04-15 19:27:03 +0300

    Update moduleloader.js

--- src/plugins/moduleloader.js
+++ src/plugins/moduleloader.js
@@ -102,7 +102,7 @@ class ModuleLoader {
    */
   async loadModule(module) {
     try {
-      const moduleName = module.name.toLowerCase();
+      const moduleName = module.uiName;
       if (this.moduleCache[moduleName]) {
         return this.moduleCache[moduleName];
       }
