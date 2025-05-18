85e6b8846a502ab326a0c428e0ee788e5b2980b1
Ersin Güvenç <eguvenc@gmail.com>
2025-05-18 21:11:23 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 // Composables
 import i18n from "@/modules/i18n/src/plugin";
 import { createRouter, createWebHistory } from "vue-router";
+import useAuth from "olobase-admin/src/store/auth";
 
 const routes = [
   {
     path: "",
+    redirect: "/dashboard",
+    meta: {
+      auth: true,
+    },
     component: () => import("@/modules/users/src/layouts/Admin.vue"),
     children: [
       {
@@ -12,6 +17,7 @@ const routes = [
         name: "dashboard",
         component: () => import('@/views/Dashboard.vue'),
         meta: {
+          auth: true,
           title: "Dashboard",
         },
       },
@@ -22,6 +28,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// Kullanıcı girişi yapılmamışsa login sayfasına yönlendir
+router.beforeEach(async (to, from, next) => {
+
+  const auth = useAuth();
+  const isAuthenticated = await auth.checkAuth();
+
+  if (to.meta?.auth && !isAuthenticated) {
+    return next({ name: "users_login" });
+  }
+
+  next();
 });
 
 export default router;
